6a7467091c425c10a08ba2382571e77173967d65
aamat <aamat@gmail.com>
2020-06-20 11:59:48 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     #viewsのpost_list関数を使いますよという意味。
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    #int型のpk（変数）がviews.pyに渡される。
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    
 ]
